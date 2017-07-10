f23e76ed9b0190e60cbc193947ab731a019ab503
Felipe Zimmerle <felipe@zimmerle.org>
2017-07-09 21:54:57 -0300

    Less verbose main loop

--- src/org/modsecurity/experimental/ModSecurityEsperMain.java
+++ src/org/modsecurity/experimental/ModSecurityEsperMain.java
@@ -1,7 +1,7 @@
 package org.modsecurity.experimental;
 
 import org.modsecurity.experimental.input.EsperHttpInputAdapter;
-import org.modsecurity.experimental.support.SupportHTTPClient;
+import org.modsecurity.experimental.utils.SupportHTTPClient;
 
 public class ModSecurityEsperMain {
 	public static boolean running = true;
@@ -11,11 +11,10 @@ public class ModSecurityEsperMain {
         test.run();
 
         SupportHTTPClient client = new SupportHTTPClient();
-        client.request(Integer.parseInt(EsperHttpInputAdapter.HTTP_PORT), "sendevent", "stream", "AccessLogEvent", "date", "start event");
+        client.request(Integer.parseInt(EsperHttpInputAdapter.HTTP_PORT), "sendevent", "stream", "LiveEvent");
         while (running) {
-        		System.out.println("loop.");
         		try {
-        			Thread.sleep(3000);
+        			Thread.sleep(1000);
         		} catch (InterruptedException e) {
         			System.out.println("Failed to process event.");
         			e.printStackTrace();
